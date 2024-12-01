bdcb2c5e65caea785fa3dcc8220b67f8e5a8ac0e
malxcdev <malxcondev@gmail.com>
2024-12-01 10:57:35 -0500

    end version one

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kumbh-sans': ['Kumbh Sans', 'sans-serif'],
+      },
+    },
+    colors: {
+      'dark-cyan': 'hsl(185, 75%, 39%)',
+      'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      'dark-gray': 'hsl(0, 0%, 59%)',
+      'white': 'hsl(0, 0%, 100%)',
+    },
   },
   plugins: [],
 }
